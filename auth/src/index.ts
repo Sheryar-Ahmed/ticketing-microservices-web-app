583b4b7537b9b9ed75611eda97f96a52cfa422dc
DESKTOP-HA8S3GL\Sheryar-Ahmed <royalsheryar505@gmail.com>
2023-11-29 15:27:33 +0500

    created mongo db pod and then connect it with the server.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import { json } from "body-parser";
 import { authRouter } from "./routes/auth";
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(json());
 
 app.use(authRouter);
 
+const start = async() => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (error) {
+    console.log("Error connecting to database 500 internal server error", error)
+  }
+};
+
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!!!!!");
 });
+
+start();
--- auth/src/routes/auth.ts
+++ auth/src/routes/auth.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { addUser, loginUser, UpdateUser, removeUser } from '../controller/auth';
 const router = express.Router();
 
-router.get('/api/users/currentuser', addUser);
+router.get('/api/users/register', addUser);
 router.post('/api/users/sign', loginUser);
 router.post('/api/users/login', UpdateUser);
 router.post('/api/users/remove', removeUser);
