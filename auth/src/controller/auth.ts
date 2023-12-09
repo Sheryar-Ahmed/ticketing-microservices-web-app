c3dc326f14ba5fb540e282cd8dead922de059805
DESKTOP-HA8S3GL\Sheryar-Ahmed <royalsheryar505@gmail.com>
2023-12-09 10:45:59 +0500

    created user registration controller

--- auth/src/controller/auth.ts
+++ auth/src/controller/auth.ts
@@ -1,5 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import { Request, Response } from "express";
+import { User } from "../model/user";
 
 const addUser = async (req: Request, res: Response) => {
     const { email, password } = req.body;
@@ -8,6 +9,21 @@ const addUser = async (req: Request, res: Response) => {
             message: "Something is missing",
         })
     }
+    const userExists = await User.exists({ email: email });
+    if (userExists) {
+        return res.status(402).json({
+            message: "User Already exists with this email. Try to register with different email."
+        });
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).json({
+        message: "User registered Successfully",
+        user: user
+    })
+
 };
 
 const loginUser = async (req: Request, res: Response) => {
