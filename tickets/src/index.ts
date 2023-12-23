c4271b90015dbc8230caea506686ed696d115cc5
DESKTOP-HA8S3GL\Sheryar-Ahmed <royalsheryar505@gmail.com>
2023-12-23 18:10:32 +0500

    built the ticket service configuration.

---
+++ tickets/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { json } from "body-parser";
+import mongoose from 'mongoose';
+import cookieSession from "cookie-session";
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  }));
+
+
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets');
+    console.log("Connected to database Successfully.")
+  } catch (error) {
+    console.log("Error connecting to database 500 internal server error", error)
+  }
+};
+
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!!!!!");
+});
+
+start();
