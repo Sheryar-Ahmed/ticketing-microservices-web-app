2a21db71dffe0d3a239971c1d4035d575358881f
sheryarahmedse <royalsheryar505@gmail.com>
2023-12-31 18:10:33 +0500

    Modified the LandingPage component to use passHref prop in the Link component, replacing the <a> tag inside the Link. Also added a unique key to each Link to optimize rendering.

--- client/components/Card.js
+++ client/components/Card.js
@@ -5,25 +5,25 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Link from 'next/link';
 
-
-export default function OutlinedCard({ title, price }) {
+export default function OutlinedCard({ title, price, id }) {
     return (
         <Box sx={{ minWidth: 275, mb: 1 }}>
-            <Card variant="outlined" title={title} price={price}>
-                <React.Fragment>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            <em>Title:</em> {title}
-                        </Typography>
-                        <Typography gutterBottom variant="h5" component="div">
-                            <em>Price:</em> ${price}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Buy Ticket</Button>
-                    </CardActions>
-                </React.Fragment>
+            <Card variant="outlined">
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" sx={{ textDecoration: 'none' }}>
+                        <em>Title:</em> {title}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="div" sx={{ textDecoration: 'none' }}>
+                        <em>Price:</em> ${price}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Link href='/tickets/[ticketId]' as={`/tickets/${id}`} >
+                        <Button size="small" sx={{ textDecoration: 'none' }}>View</Button>
+                    </Link>
+                </CardActions>
             </Card>
         </Box>
     );
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,24 @@
 import { Box, Typography } from '@mui/material';
 import OutlinedCard from '../components/Card';
+import Link from 'next/link';
 
 const LandingPage = ({ currentUser, tickets }) => {
-    return (
-        <Box sx={{ mx: 5, my: 10 }}>
-            <Typography spacing={5} sx={{ mb: 3 }} variant="h4" component="h2" align="center">
-                Tickets for Sale
-            </Typography>
-            {tickets && tickets.map((item) => <OutlinedCard title={item.title} price={item.price} />)}
-        </Box>
-    );
+  return (
+    <Box sx={{ mx: 5, my: 10 }}>
+      <Typography spacing={5} sx={{ mb: 3 }} variant="h4" component="h2" align="center">
+        Tickets for Sale
+      </Typography>
+
+      {tickets && tickets.map(({title, price, id}) => (
+            <OutlinedCard key={id} id={id} title={title} price={price} />
+      ))}
+    </Box>
+  );
 }
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-    const { data } = await client.get('/api/ticket/getall');
-    return { tickets: data.alltickets };
+  const { data } = await client.get('/api/ticket/getall');
+  return { tickets: data.alltickets };
 }
 
 export default LandingPage;
