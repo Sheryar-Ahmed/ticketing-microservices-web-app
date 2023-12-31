d8f9b3dc0c74ea75a4f805cd4ea72f2ac1772741
sheryarahmedse <royalsheryar505@gmail.com>
2023-12-31 17:47:36 +0500

    Integrated Tickets with the components and displayed on the landing page.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,20 @@
-import buildClient from '../api/build-client';
+import { Box, Typography } from '@mui/material';
+import OutlinedCard from '../components/Card';
 
-const LandingPage = ({ currentUser }) => {
-    return currentUser ? <h1>User Logged In </h1> : <h1>User is Not logged in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+    return (
+        <Box sx={{ mx: 5, my: 10 }}>
+            <Typography spacing={5} sx={{ mb: 3 }} variant="h4" component="h2" align="center">
+                Tickets for Sale
+            </Typography>
+            {tickets && tickets.map((item) => <OutlinedCard title={item.title} price={item.price} />)}
+        </Box>
+    );
 }
 
-LandingPage.getInitialProps = async context => {
-    return {};
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+    const { data } = await client.get('/api/ticket/getall');
+    return { tickets: data.alltickets };
 }
-
 
 export default LandingPage;
