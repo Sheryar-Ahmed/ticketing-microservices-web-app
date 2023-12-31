dedf144df0888d50c8f21f0ea7099c5084e71dd3
sheryarahmedse <royalsheryar505@gmail.com>
2023-12-31 19:18:59 +0500

    got order SSR

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,17 @@
 
-const OrderShow = () => {
+const OrderShow = ({ order }) => {
+    console.log("order", order)
     return <div>
-        Order Show
+        Order Show {order.id}
     </div>
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+    const { orderId } = context.query;
+    console.log("orderid", orderId);
+    const { data } = await client.get(`/api/orders/getone/${orderId}`);
+
+    return { order: data.existingOrder };
 }
 
 
